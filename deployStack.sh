0c1b3cfe684cd716a63dfc107ef62f8cc1441c9c
Michael Kade <mike@mikda.com>
2020-01-22 19:34:21 -0700

    Changed the deployStack.sh to address incorrect node name

--- deployStack.sh
+++ deployStack.sh
@@ -3,8 +3,8 @@
 export ELASTIC_VERSION=7.5.0
 export ELASTICSEARCH_USERNAME=qumulo
 export ELASTICSEARCH_PASSWORD=changeme
-export ELASTICSEARCH_HOST=dq2-a-40g
-export INITIAL_MASTER_NODES=dq2-a-40g
+export ELASTICSEARCH_HOST=NODE1
+export INITIAL_MASTER_NODES=NODE1
 
 docker network create --driver overlay --attachable qumulo
 docker stack deploy --compose-file docker-compose.yml qumulo
